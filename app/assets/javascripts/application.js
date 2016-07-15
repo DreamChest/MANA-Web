a90ced557de6fe36aaa1aa744ace830bdb0405fe
Sudiukil <sudiukil@gmx.fr>
2016-07-15 21:27:34 +0200

    Modified entries sorting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require bootstrap
 //= require selectize
+//= require jscolor.min
+//= require turbolinks
 //= require_tree .
