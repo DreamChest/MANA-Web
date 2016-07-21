0e582af8d20fb34d934f3659d0e4cd464b9883b0
Sudiukil <sudiukil@gmx.fr>
2016-07-21 19:03:51 +0200

    Fully reviewed tag filtering and sidebar design (tag filtering added to sidebar)

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,20 +1,27 @@
 // Adds all needed listeners (and stuff)
 function init() {
-	$(".tag_cb").on("change", handle_tags);
+	tag_filtering();
 }
 
-// Handles tags filtering (entries index)
-function handle_tags() {
-	var tags = $(".tag_cb").filter(":checked");
+// Handles tag filtering (entries index)
+function tag_filtering() {
+	var args = location.search.split("tags=");
 
-	if(tags.size()>0) {
-		$(".panel").hide();
-		tags.each(function() { $(".panel").filter("."+this.name).show(); });
-	}
-	else {
-		$(".panel").show();
+	if(args.length>1) {
+		var tags = args[1].split(",");
+
+		if(tags.length>0) {
+			$(".panel").hide();
+			for(var i = 0; i<tags.length; i++) {
+				$(".panel").filter("."+tags[i]).show();
+			}
+		}
+		else {
+			$(".panel").show();
+		}
 	}
 }
+
 
 // Handles panel collapses (entries index)
 function handle_collapses() {
