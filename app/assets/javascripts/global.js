f28328c10999265306f1d76f28c55e2a087926fc
Sudiukil <sudiukil@gmx.fr>
2017-02-19 17:36:50 +0100

    Fixed not updated sidebars when adding/updating/deleting sources or tags

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -21,18 +21,31 @@ function dynamic_modal_load() {
 	return false;
 }
 
+// Loads page-content and sidebars contents from ajax result
+function dynamic_full_load(result) {
+	$("#page-content").html($(result).find("#page-content").html());
+	$("#sidebar-wrapper").html($(result).find("#sidebar-wrapper").html());
+	$("#sidebar-wrapper-right").html($(result).find("#sidebar-wrapper-right").html());
+}
+
 // Dynamically loads a link into the page sidebars and page content (for delete http method)
 function dynamic_delete_load() {
 	$.ajax({
 		url: $(this).attr("href"),
 		type: "DELETE",
-		success: function(result) {
-			$("#page-content").html($(result).find("#page-content").html());
-			$("#sidebar-wrapper").html($(result).find("#sidebar-wrapper").html());
-			$("#sidebar-wrapper-right").html($(result).find("#sidebar-wrapper-right").html());
-		}
+		success: dynamic_full_load
 	});
 	return false;
+}
+
+// Dynamicaly load back link after form submission
+function dynamic_form_load() {
+	$.ajax({
+		url: $("#back").attr("href"),
+		type: "GET",
+		success: dynamic_full_load
+	});
+	$("#page-modal").modal("hide");
 }
 
 // Yup, that's dirty, but temporary (maybe)
