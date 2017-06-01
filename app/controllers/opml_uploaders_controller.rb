a2734d40a957735d2fcd3d756fa3b9598c945c94
Sudiukil <sudiukil@gmx.fr>
2017-06-01 02:22:05 +0200

    Code cleanup for OPML uploader, notices improvement, localization improvement

--- app/controllers/opml_uploaders_controller.rb
+++ app/controllers/opml_uploaders_controller.rb
@@ -1,24 +1,10 @@
 class OpmlUploadersController < ApplicationController
 	before_action :set_opml_uploader, only: [:show, :edit, :update, :destroy]
-
-	# GET /opml_uploaders
-	# GET /opml_uploaders.json
-	def index
-		@opml_uploaders = OpmlUploader.all
-	end
-
-	# GET /opml_uploaders/1
-	# GET /opml_uploaders/1.json
-	def show
-	end
+	after_action :destroy_opml_uploader, only: [:create]
 
 	# GET /opml_uploaders/new
 	def new
 		@opml_uploader = OpmlUploader.new
-	end
-
-	# GET /opml_uploaders/1/edit
-	def edit
 	end
 
 	# POST /opml_uploaders
@@ -33,19 +19,29 @@ class OpmlUploadersController < ApplicationController
 
 				doc = Nokogiri::XML(File.open(@opml_uploader.file.path))
 
+				new_sources = 0 # Count for new sources
+				ignored_sources = 0 # Count for ignore sources (already existent)
+
 				doc.xpath("//outline[@type='tag']").each do |xtag| # For each tag in OPML file
 					tagName = xtag.xpath("@text").text # Tag name from OPML
+
 					tag = Tag.where("name = ?", tagName).take # Pull tag from DB (if it exists)
 					tag = tag ? tag : Tag.create(name: tagName, color: "#ffffff") # Keep tag if it exists in DB, else create it
-					puts "Tag #{tagName} has:"
+
 					xtag.xpath("outline[@type='rss']").each do |xsource| # For each source of the tag in OPML file
 						sourceName = xsource.xpath("@text").text # Source name from OPML
 						sourceXmlUrl = xsource.xpath("@xmlUrl").text # Source XML URL from OPML
 						sourceHtmlUrl = xsource.xpath("@htmlUrl").text # Source HTML URL from OPML
+
 						source = Source.where("url = ?", sourceXmlUrl).take # Pull source from DB (if it exists)
-						source = source ? source : Source.create(name: sourceName, url: sourceXmlUrl, html_url: sourceHtmlUrl, last_update: Time.at(0)) # Keep source if it exists in DB, else create it
+						if source.nil? # If source does not exist...
+							source = Source.create(name: sourceName, url: sourceXmlUrl, html_url: sourceHtmlUrl, last_update: Time.at(0)) # ... we create it.
+							new_sources += 1
+						else
+							ignored_sources += 1
+						end
+
 						source.tags<<(tag) unless source.tags.exists?(tag.id) # Tag source with tag (unless it is already tagged)
-						puts "\tSource: #{sourceName}, #{sourceXmlUrl}, #{sourceHtmlUrl}"
 					end
 				end
 
@@ -54,7 +50,13 @@ class OpmlUploadersController < ApplicationController
 					render "sources/index"
 				}
 				format.json {
-					flash[:notice] = I18n.t("notices.opml_imported")
+					flash[:notice] = I18n.t("notices.opml_imported",
+											new_count: new_sources,
+											new_string: t("words.new_f", count: new_sources).downcase,
+											source_string: t("words.source", count: new_sources).downcase,
+											ignored_count: ignored_sources,
+											ignored_string: t("words.ignored_source", count: ignored_sources))
+
 					render :show, status: :created, location: @opml_uploader
 				}
 			else
@@ -64,31 +66,13 @@ class OpmlUploadersController < ApplicationController
 		end
 	end
 
-	# PATCH/PUT /opml_uploaders/1
-	# PATCH/PUT /opml_uploaders/1.json
-	def update
-		respond_to do |format|
-			if @opml_uploader.update(opml_uploader_params)
-				format.html { redirect_to @opml_uploader, notice: 'Opml uploader was successfully updated.' }
-				format.json { render :show, status: :ok, location: @opml_uploader }
-			else
-				format.html { render :edit }
-				format.json { render json: @opml_uploader.errors, status: :unprocessable_entity }
-			end
-		end
-	end
-
-	# DELETE /opml_uploaders/1
-	# DELETE /opml_uploaders/1.json
-	def destroy
-		@opml_uploader.destroy
-		respond_to do |format|
-			format.html { redirect_to opml_uploaders_url, notice: 'Opml uploader was successfully destroyed.' }
-			format.json { head :no_content }
-		end
-	end
-
 	private
+	# Destroy the OPML uploader (and the uploaded file) after it's creation
+	# (Used because we don't want to store the file nor keep the uploader, since we only want to parse the OPML file)
+	def destroy_opml_uploader
+		OpmlUploader.all.each do |o| o.destroy end # Actually, make sure that ALL OPML uploaders are destroyed
+	end
+
 	# Use callbacks to share common setup or constraints between actions.
 	def set_opml_uploader
 		@opml_uploader = OpmlUploader.find(params[:id])
--- app/views/opml_uploaders/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@opml_uploaders) do |opml_uploader|
-  json.extract! opml_uploader, :id
-  json.url opml_uploader_url(opml_uploader, format: :json)
-end
