1772bedc4f1a02a1c1a3f86ff50a6cc452ac1347
Sudiukil <sudiukil@gmx.fr>
2017-06-04 17:44:34 +0200

    Minor bug fixes and improvements

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require "fileutils"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,9 @@ module MANAWeb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+	config.after_initialize do
+		FileUtils::mkdir "public/assets/favicons" unless File.exists?("public/assets/favicons")
+	end
   end
 end
